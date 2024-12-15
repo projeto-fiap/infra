487982f3cca9fcc61caa36e59681a6937e269731
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2024-12-15 14:22:58 -0300

    add regra PostgreSQL

--- terraform-config/sg.tf
+++ terraform-config/sg.tf
@@ -3,6 +3,7 @@ resource "aws_security_group" "sg" {
   description = "Grupo usado para o cluster EKS"
   vpc_id      = data.aws_vpc.vpc.id
 
+  # Regra de entrada HTTP
   ingress {
     description = "All"
     from_port   = 80
@@ -11,11 +12,29 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Regra de entrada PostgreSQL
+  ingress {
+    description = "PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Regra de Egresso
   egress {
     description = "All"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Regra de entrada PostgreSQL
+  egress {
+    description = "PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
